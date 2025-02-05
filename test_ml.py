c97249fd5d5dae2e2032d14eaf88d0b3f4eda011
vandech <ndechi2009@gmail.com>
2025-02-04 21:03:45 -0600

    Changes to the test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,80 @@
 import pytest
+import os
+import logging
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.exceptions import NotFittedError
+
+from ml.model import inference, compute_model_metrics
+from ml.data import process_data
+
+
+
+
 # TODO: add necessary import
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_features(data, features):
     """
     # add description for the first test
+    Check that categorical features are in dataset
     """
     # Your code here
-    pass
+    try:
+        assert sorted(set(data.columns).intersection(features)) == sorted(features)
+    except AssertionError as err:
+        logging.error(
+            "Testing dataset: Features are missing in the data columns")
+        raise err
+
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_inference(train_dataset):
     """
     # add description for the second test
+    Check inference function
     """
     # Your code here
-    pass
+
+    X_train, y_train = train_dataset
+
+    savepath = "./model/trained_model.pkl"
+    if os.path.isfile(savepath):
+        model = pickle.load(open(savepath, 'rb'))
+
+        try:
+            preds = inference(model, X_train)
+        except Exception as err:
+            logging.error(
+            "Inference cannot be performed on saved model and train data")
+            raise err
+    else:
+        pass
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics(train_dataset):
     """
     # add description for the third test
+
+    Check calculation of performance metrics function
     """
     # Your code here
-    pass
+
+    X_train, y_train = train_dataset
+
+    savepath = "./model/trained_model.pkl"
+    if os.path.isfile(savepath):
+        model = pickle.load(open(savepath, 'rb'))
+        preds = inference(model, X_train)
+
+        try:
+            precision, recall, fbeta = compute_model_metrics(y_train, preds)
+        except Exception as err:
+            logging.error(
+            "Performance metrics cannot be calculated on train data")
+            raise err
+    else:
+        pass
